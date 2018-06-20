b815f8b38bf7631151b746da080111a2314a4c55
Julian Lechuga <julian.lechuga305@gmail.com>
2018-06-20 17:13:15 +0900

    Initial Commit

---
+++ Floor_lava/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Floor_lava
+//
+//  Created by Julian Lechuga Lopez on 20/6/18.
+//  Copyright © 2018 Julian Lechuga Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
