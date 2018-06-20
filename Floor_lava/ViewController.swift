ea924787e084e5255b95dab64d80965fd53e93da
Julian Lechuga <julian.lechuga305@gmail.com>
2018-06-20 17:17:42 +0900

    First commit for initialization of project

--- Floor_lava/ViewController.swift
+++ Floor_lava/ViewController.swift
@@ -7,11 +7,15 @@
 //
 
 import UIKit
-
+import ARKit
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.sceneView.session.run(configuration)
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
         // Do any additional setup after loading the view, typically from a nib.
     }
 
