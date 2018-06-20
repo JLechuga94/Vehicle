e23be620c901d8d85c19a0f692e472381e0b2cbb
Julian Lechuga <julian.lechuga305@gmail.com>
2018-06-20 18:13:42 +0900

    planeAnchor of horizontal detected surfaces with lava diffuse

--- Floor_lava/ViewController.swift
+++ Floor_lava/ViewController.swift
@@ -8,17 +8,47 @@
 
 import UIKit
 import ARKit
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet weak var sceneView: ARSCNView!
     let configuration = ARWorldTrackingConfiguration()
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.sceneView.session.run(configuration)
         self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.configuration.planeDetection = .horizontal
+        self.sceneView.session.run(configuration)
+        self.sceneView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func createLava(planeAnchor: ARPlaneAnchor) -> SCNNode{
+        let planeAnchorPosition = planeAnchor.center
+        let lavaNode = SCNNode(geometry: SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z)))
+        lavaNode.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "Lava")
+        lavaNode.geometry?.firstMaterial?.isDoubleSided = true
+        lavaNode.eulerAngles = SCNVector3(Float(90.degreesToRadians),0,0)
+        lavaNode.position = SCNVector3(planeAnchorPosition.x, planeAnchorPosition.y, planeAnchorPosition.z)
+        return lavaNode
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        let lavaNode = createLava(planeAnchor: planeAnchor)
+        print("new flat surface detected, new ARPlaneAnchor added")
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        print("*************")
+        print("updating floors anchor")
+    }
+    
+//    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+//        <#code#>
+//    }
 
 
 }
-
+extension Int {
+    var degreesToRadians: Double { return Double(self) * .pi/180}
+}
